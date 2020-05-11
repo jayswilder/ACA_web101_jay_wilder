d238299fba7587f506d4e94420ba2cddd9bd7f1d
Jay Wilder <jayswilder@yahoo.com>
2020-05-10 22:24:33 -0500

    added js to resume & CSS animatons

---
+++ resume-interactive.js
@@ -0,0 +1,10 @@
+const showNumber = () => {
+    document.getElementById("number").innerHTML = "(04) 209 198 298"
+}
+
+const showNumber2 = () => {
+    document.getElementById("number2").innerHTML = "+76 1981 2988 3771"
+}
+
+
+
